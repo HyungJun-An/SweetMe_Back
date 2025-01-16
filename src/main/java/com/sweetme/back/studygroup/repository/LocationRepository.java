c87949158ddd879fac2cc25bebf6c394c87d4180
AHJ <121944891+HyungJun-An@users.noreply.github.com>
2025-01-16 19:07:02 +0900

    Feat: JPA Entity 별 JPA 기본 Repository 생성 (#5)
    * Feat: JPA Entity 별 JPA 기본 Repository 생성
    
    * Feat: JPA User Entity User Repository 추가 생성
    
    ---------
    
    Co-authored-by: 안형준 <anhyeongjun@anhyeongjun-ui-MacBookAir.local>

---
+++ src/main/java/com/sweetme/back/studygroup/repository/LocationRepository.java
@@ -0,0 +1,8 @@
+package com.sweetme.back.studygroup.repository;
+
+import com.sweetme.back.studygroup.domain.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}
